fe91b9aedb9f87a2c7c48003e3cc84c96ec212ee
Oskar Mikus <oskarmikus@protonmail.com>
2021-11-18 00:48:01 +0100

    Limited cors

--- main.go
+++ main.go
@@ -12,9 +12,16 @@ import (
 	"os"
 )
 
-func CorsMiddleware(next http.Handler) http.Handler {
+func CorsMiddleware(next http.Handler, mode string) http.Handler {
+	var origin string
+	if mode == "dev" {
+		origin = os.Getenv("DEV_ORIGIN")
+	} else {
+		origin = os.Getenv("PROD_ORIGIN")
+	}
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Request-Method", "POST")
 		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 		next.ServeHTTP(w, r)
 	})
@@ -23,12 +30,20 @@ func CorsMiddleware(next http.Handler) http.Handler {
 func main() {
 	_ = godotenv.Load()
 	port := os.Getenv("PORT")
+	var mode string
+	if os.Args[1] == "--dev" {
+		mode = "dev"
+	} else {
+		mode = "prod"
+	}
+
+	log.Printf("Server running in %s mode\n", mode)
 	service.InitDb()
 	defer service.Disconnect()
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", CorsMiddleware(srv))
+	http.Handle("/query", CorsMiddleware(srv, mode))
 	log.Printf("GraphiQL http://localhost:%s/", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
