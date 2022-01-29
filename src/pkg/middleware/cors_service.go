07f3247711085a486f254d9222096db3b197071c
Oskar Mikus <oskarmikus@protonmail.com>
2022-01-29 21:51:52 +0100

    edit origin

--- src/pkg/middleware/cors_service.go
+++ src/pkg/middleware/cors_service.go
@@ -6,7 +6,7 @@ import (
 
 func Cors(next http.Handler, mode string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "https://lembook.netlify.app")
 		w.Header().Set("Access-Control-Request-Method", "POST, GET")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
