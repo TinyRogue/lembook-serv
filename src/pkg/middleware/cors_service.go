d9637a1b0de367d5ceebcbf80d8a61ea026a4ec9
Oskar Mikus <oskarmikus@protonmail.com>
2022-01-29 21:38:29 +0100

    edit origin

--- src/pkg/middleware/cors_service.go
+++ src/pkg/middleware/cors_service.go
@@ -2,18 +2,11 @@ package middleware
 
 import (
 	"net/http"
-	"os"
 )
 
 func Cors(next http.Handler, mode string) http.Handler {
-	var origin string
-	if mode == "dev" {
-		origin = os.Getenv("DEV_ORIGIN")
-	} else {
-		origin = os.Getenv("PROD_ORIGIN")
-	}
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Request-Method", "POST, GET")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
